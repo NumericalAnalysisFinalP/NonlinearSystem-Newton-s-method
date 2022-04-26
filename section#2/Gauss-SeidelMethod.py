743d9b7370b89dfb9674a0e2ed94f4dde70a2dec
Zahraa Al <zalshala@mail.sfsu.edu>
2022-04-25 19:50:57 -0700

    fixed Gauss-Seidel Method code

--- section#2/Gauss-SeidelMethod.py
+++ section#2/Gauss-SeidelMethod.py
@@ -1,33 +1,68 @@
 from numpy import array
 from numpy import linalg as LA
 from numpy import inf
+import numpy as np
 import copy
 
 # given linear system
 
-A = array([[4.0, -1, 0, -2, 0, 0], [-1, 4, -1, 0, -2, 0], [0, -1, 4, 0, 0, -2], [-1, 0, 0, 4, -1, 0], [0, -1, 0, -1, 4, -1], [0, 0, -1, 0, -1, 4]])
+A = array([
+ [4.0, -1, 0, -2, 0, 0],
+ [-1, 4, -1, 0, -2, 0], 
+ [0, -1, 4, 0, 0, -2], 
+ [-1, 0, 0, 4, -1, 0], 
+ [0, -1, 0, -1, 4, -1], 
+ [0, 0, -1, 0, -1, 4]])
 b = array([-1.0,0,1,-2,1,2])
 
-m = len(b) # return the array is the same length as b
-x = array([0.0,0,0,0,0,0]) #initial vector
+#first check if the coefficient matrix is diagonally dominant or not
+
+# Find diagonal coefficients
+diag = np.diag(np.abs(A)) 
+
+# Find row sum without diagonal
+off_diag = np.sum(np.abs(A), axis=1) - diag 
+
+if np.all(diag >= off_diag):
+    print('matrix is diagonally dominant')
+else:
+    print('NOT diagonally dominant')
+
+#m = len(b) # return the array is the same length as b
+x1 = 0
+x2 = 0
+x3 = 0
+x4 = 0
+x5 = 0
+x6 = 0
+epsilon = 0.01
 tol = 5*10**-6 # iteration max limit
+converged = False
 
-n = 0 # iteration counter
-xn = 1 # initial tolerance
+x_old = np.array([x1, x2, x3, x4, x5, x6])
 
-while xn > tol:
-    x0 = copy.copy(x)
-    x[0] = (b[0] - ( A[0,1]*x[1]) )/A[0,0]
+print('Iteration results')
+print('k,    x1,     x2,     x3,     x4,      x5,      x6')
+for k in range(1, 50):
+    x1 = (-1+x2+2*x4)/(4)
+    x2 = (x1+x3+2*x5)/(4)
+    x3 = (1+x2+2*x6)/(4)
+    x4 = (-2+x1+x5)/(4)
+    x5 = (1+x2+x4+x6)/(4)
+    x6 = (2+x3+x5)/(4)
+    x = np.array([x1, x2, x3, x4, x5, x6])
 
-    for i in range(1,m-1):
-        x[i] = ( b[i]- ( A[i, i-1]*x[i-1] + A[i,i+1]*x[i+1] ) )/A[i,i]
+    # check if it is smaller than threshold
+    dx = np.sqrt(np.dot(x-x_old, x-x_old))
+
+    print("%d, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f"%(k, x1, x2, x3, x4, x5, x6))
+    if dx < epsilon:
+        converged = True
+        print('Converged!')
+        break
+    x_old = x
+if not converged:
+    print('Not converge, increase the # of iterations')
         
-    x[m-1] = (b[m-1] - ( A[m-1,m-2]*x[m-2] ) )/A[m-1,m-1]
-   
-    #xn = LA.norm(x-x0,1) 
-    #xn = LA.norm(x-x0,2) 
-    xn = LA.norm(x-x0,inf)
-    n = n + 1
-
-print(f" Number of iterations for Gauss-Seidel = {n}")  
+print(f"Number of iterations for Gauss-Seidel = {k}")  
 print(f"approximate x = {x}")      
