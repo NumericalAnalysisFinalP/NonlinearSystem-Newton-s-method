f8efc840c8406f918b56598b151f8be61d44f781
Zahraa Al <zalshala@mail.sfsu.edu>
2022-04-29 15:36:34 -0700

    changed folder name

--- Section2/Gauss-SeidelMethod.py
+++ Section2/Gauss-SeidelMethod.py
@@ -3,6 +3,21 @@ from numpy import linalg as LA
 from numpy import inf
 import numpy as np
 
+"""
+%Gauss-Seidel   Use both the Jacobi method and the Gauss-Seidel method to solve the indicated linear system of equations.
+%             
+%             
+%
+%     inputs:
+%             A       coefficient matrix for linear system
+%                     (matrix must be square)
+%             b       right-hand side vector
+%
+%     output:
+%             x       solution vector (i.e., vector for which Ax = b)
+%
+%
+"""
 
 ITERATION_LIMIT = 1000
 
@@ -14,20 +29,25 @@ A = array([
     [-1, 0, 0, 4, -1, 0], 
     [0, -1, 0, -1, 4, -1], 
     [0, 0, -1, 0, -1, 4]])
-# initialize the RHS vector
+# initialize the b vector
 b = array([-1.0,0,1,-2,1,2])
 
 n  = 0  #iteration counter
 
+# prints the equations of the linear system (input)
 print("System of equations:")
 for i in range(A.shape[0]):
     row = ["{0:3g}*x{1}".format(A[i, j], j + 1) for j in range(A.shape[1])]
     print("[{0}] = [{1:3g}]".format(" + ".join(row), b[i]))
 
+# initial vector
 x = np.zeros_like(b)
+
+# Gauss-Seidel method
 for it_count in range(1, ITERATION_LIMIT):
     x_new = np.zeros_like(x)
     print("Iteration {0}: {1}".format(it_count, x))
+    
     for i in range(A.shape[0]):
         s1 = np.dot(A[i, :i], x_new[:i])
         s2 = np.dot(A[i, i + 1 :], x[i + 1 :])
@@ -36,11 +56,12 @@ for it_count in range(1, ITERATION_LIMIT):
         break
     x = x_new
 
-    # Number of iterations for Jacobi
+    # Number of iterations for Gauss-Seidel method
     xn = LA.norm(x-it_count,inf)
     n = n + 1
 
-print("Solution: {0}".format(x))
+# Output 
+print("approximate x = {0}".format(x))
 error = np.dot(A, x) - b
 print("Error: {0}".format(error))
 print(f" Number of iterations for Gauss-Seidel = {n}") 
--- Section2/JacobiMethod.py
+++ Section2/JacobiMethod.py
@@ -3,28 +3,51 @@ from numpy import array
 from numpy import linalg as LA
 from numpy import inf
 
+"""
+%Jacobi      Use both the Jacobi method and the Gauss-Seidel method to solve the indicated linear system of equations.
+%             
+%   
+%             
+%
+%     inputs:
+%             A       coefficient matrix for linear system
+%                     (matrix must be square)
+%             b       right-hand side vector
+%
+%     output:
+%             x       solution vector (i.e., vector for which Ax = b)
+%
+%
+"""
+# setting the iteration limit 
+
 ITERATION_LIMIT = 1000
 
 # initialize the matrix
-A = array(
-    [[4.0, -1, 0, -2, 0, 0], 
+A = array([
+    [4.0, -1, 0, -2, 0, 0],
     [-1, 4, -1, 0, -2, 0], 
     [0, -1, 4, 0, 0, -2], 
     [-1, 0, 0, 4, -1, 0], 
     [0, -1, 0, -1, 4, -1], 
     [0, 0, -1, 0, -1, 4]])
+# initialize the b vector
 b = array([-1.0,0,1,-2,1,2])
 
 
-n = 0
-# prints the system
-print("System:")
+n = 0 #iteration counter
+
+# prints the equations of the linear system (input)
+print("System of equations:")
 for i in range(A.shape[0]):
     row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
     print(f'{" + ".join(row)} = {b[i]}')
 print()
 
+# initial vector
 x = np.zeros_like(b)
+
+# Jacobi method
 for it_count in range(ITERATION_LIMIT):
     if it_count != 0:
         print("Iteration {0}: {1}".format(it_count, x))
@@ -43,12 +66,11 @@ for it_count in range(ITERATION_LIMIT):
     x = x_new
 
     # Number of iterations for Jacobi
-
     xn = LA.norm(x-it_count,inf)
     n = n + 1
 
-print("Solution: ")
-print(x)
+# Output
+print("approximate x = {0}".format(x))
 error = np.dot(A, x) - b
 print("Error:")
 print(error)
