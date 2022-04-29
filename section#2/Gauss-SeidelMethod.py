e57722b7f4750f997e7fa1ea671a8f3e497d7562
Zahraa Al <zalshala@mail.sfsu.edu>
2022-04-29 13:36:37 -0700

    finished section 2

--- section#2/Gauss-SeidelMethod.py
+++ section#2/Gauss-SeidelMethod.py
@@ -4,6 +4,7 @@ from numpy import inf
 import numpy as np
 import copy
 
+
 # given linear system
 
 A = array([
@@ -28,21 +29,28 @@ if np.all(diag >= off_diag):
 else:
     print('NOT diagonally dominant')
 
-#m = len(b) # return the array is the same length as b
+m   = len(b)
+x   = array([0.0,0,0,0,0,0]) # initial vector
+tol = 5e-6
+
 x1 = 0
 x2 = 0
 x3 = 0
 x4 = 0
 x5 = 0
 x6 = 0
-epsilon = 5*10**-6 # iteration max limit
 converged = False
 
-x_old = np.array([x1, x2, x3, x4, x5, x6])
+n  = 0  #iteration counter
+x = np.array([x1, x2, x3, x4, x5, x6])
+xn = 1 # dummy initial tolerance
 
 print('Iteration results')
 print('k,    x1,     x2,     x3,     x4,      x5,      x6')
-for k in range(1, 50):
+
+while xn > tol:
+
+    x0 = copy.copy(x) 
     x1 = (-1+x2+2*x4)/(4)
     x2 = (x1+x3+2*x5)/(4)
     x3 = (1+x2+2*x6)/(4)
@@ -50,18 +58,22 @@ for k in range(1, 50):
     x5 = (1+x2+x4+x6)/(4)
     x6 = (2+x3+x5)/(4)
     x = np.array([x1, x2, x3, x4, x5, x6])
-
+    
+    #xn = LA.norm(x-x0,1) 
+    #xn = LA.norm(x-x0,2) 
+    xn = LA.norm(x-x0,inf)
+    n = n + 1
+    
     # check if it is smaller than threshold
-    dx = np.sqrt(np.dot(x-x_old, x-x_old))
-
-    print("%d, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f"%(k, x1, x2, x3, x4, x5, x6))
-    if dx < epsilon:
+    dx = np.sqrt(np.dot(x-x0, x-x0))
+    print("%d, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f"%(n, x1, x2, x3, x4, x5, x6))
+    if dx < tol:
         converged = True
         print('Converged!')
         break
-    x_old = x
+    x0 = x
 if not converged:
     print('Not converge, increase the # of iterations')
-        
-print(f"Number of iterations for Gauss-Seidel = {k}")  
-print(f"approximate x = {x}")      
+          
+print(f" Number of iterations for Gauss-Seidel = {n}")  
+print(f"approximate x = {x}")
